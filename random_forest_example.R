f1e68b38bf0955f1216422d2d2aaa953d8508db2
Krämer <mkraemer@diw-berlin.de>
2021-07-13 19:34:08 +0200

    random forest: variable importance

--- random_forest_example.R
+++ random_forest_example.R
@@ -16,10 +16,11 @@ library("varSelRF")
 cov.vs1 <- varSelRF(covmerged_red[1:30], covmerged$ev_gesamt_fac, ntree = 500, ntreeIterat = 300,
                    vars.drop.frac = 0.2)
 
+library("randomForest")
 # https://www.rdocumentation.org/packages/randomForest/versions/4.6-14/topics/importance
 cov.rf <- randomForest(ev_gesamt_fac ~ ., data=covmerged, ntree=1000,
                           keep.forest=FALSE, importance=TRUE)
 importance(cov.rf)
 
 imp_cov <- as.data.frame(importance(cov.rf))
-imp_cov %>% arrange(desc(MeanDecreaseGini))
+imp_cov %>% arrange(desc(MeanDecreaseGini)) %>% slice(1:20)
